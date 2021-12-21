0493d1f83c47a90db1c383c1153430db594bc268
Edgar Fernandez <edgar.fernand@outlook.com>
2021-12-20 21:29:31 -0800

    adding the user seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,41 +4,42 @@ const userData = [
   {
     name: 'Adrew Garfunkle',
     adress: '1 Ingroom St.',
-    phone_number: '9175564568',
+    phone_number: 9175564568,
     email: 'Spoodermon@gmail.com',
     password:'notthelamespiderman1',
-    isAdmin: 'true' 
+    isAdmin: true,
   },
   {
     name:'Tober McGoore',
     address:'2 Ingrown St.',
-    phone_number:'7185648659',
+    phone_number:7185648659,
     email:'iamfirstspooderman@gmail.com',
     password:'ishootwebsinsideme1',
-    isAdmin: 'true'
+    isAdmin: true,
   },
   {
     name:'Tim Hoyander',
     address:'19 ingram st',
-    phone_number:'3478791236',
+    phone_number:3478791236,
     email:'ididnotkillmysterio@gmail.com',
     password:'tonystarkchild3000',
-    isAdmin:'true'
+    isAdmin:true,
   },
   {
     name:'Zendoyer Jones',
     address:'59 brooklyn St',
-    phone_number:'3478859945',
+    phone_number:3478859945,
     email:'iamcollege@gmail.com',
     password:'dontmakemepressthisbutton63',
-    isAdmin:'false'
+    isAdmin:false,
   },
   {
     name:'Mary Tomelli',
     address:'19 ingram st',
-    phone_number:'3478791237',
+    phone_number:3478791237,
     email:'withhappydays@gmail.com',
     password:'withgreatpower99',
+    isAdmin:false,
   },
   
 
