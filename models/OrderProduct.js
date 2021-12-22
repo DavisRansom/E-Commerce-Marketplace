bd300fb8992a79613881e3337c54509483c5ffc2
Davis Ransom <davisransom.vb@gmail.com>
2021-12-21 20:30:38 -0800

    Update OrderProduct bridge model

--- models/OrderProduct.js
+++ models/OrderProduct.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const Order = require ('./Order');
+const Product = require ('./Product');
 
 class OrderProduct extends Model { }
 
