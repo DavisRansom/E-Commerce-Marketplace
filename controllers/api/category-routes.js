a9d5924c8523933f7baca2ecbe39bee8eeb3677e
Joseph Spann <joespann91@gmail.com>
2021-12-26 13:00:23 -0800

    Changed CSS for main homepage

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{model: Product}]
     })
-    
+    //Add render for Category handlebars page here and pass in serialized Category data
     res.status(200).json(categoryData)
   } catch (err) {
     res.status(500).json(err)
