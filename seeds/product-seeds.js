00a5128710b3e4dc9365c54ca9553e121fe756c3
Davis Ransom <davisransom.vb@gmail.com>
2022-01-03 21:03:05 -0800

    Added Stripe secret key

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,15 +2,15 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    description: 'Test', 
+    description: 'The iPhone 13 and iPhone 13 mini iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch has been reduced in size, and the rear camera module now sits at a diagonal. In addition, the A15 processor brings more speed and efficiency to every task.', 
     product_img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1631220221000',  
-    alt: 'Test alternative text',
+    alt: 'iPhone 13',
     product_name: 'iPhone 13',
     price: 1200,
     stock: 14,
   },
   {
-    description: 'Test',
+    description: 'The 11-inch and 12.9-inch iPad Pro models were updated in April 2021 with a faster M1 chip, a Liquid Retina XDR mini-LED display on the 12.9-inch model, 5G connectivity, and more. There is also a new Magic Keyboard white color option, and the new iPad Pro models continue to work with the Apple Pencil 2.',
     product_img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-12-11-select-202104_FMT_WHH?wid=2000&hei=2000&fmt=jpeg&qlt=80&.v=1617067383000',
     alt: 'Test alternative text',
     product_name: 'iPad Pro',
@@ -18,7 +18,7 @@ const productData = [
     stock: 25,
   },
   {
-    description: 'Test',
+    description: 'HP Spectre X360 is a Windows 10 Professional laptop with a 13.30-inch display that has a resolution of 1920x1080 pixels. It is powered by a Core i7 processor and it comes with 8GB of RAM. The HP Spectre X360 packs 256GB of SSD storage. Graphics are powered by Intel Integrated UHD Graphics 620.',
     product_img: 'https://cdn11.bigcommerce.com/s-je364a6h/images/stencil/608x608/products/215001/182569/85798759_6753257486__32868.1639776171.png?c=2',
     alt: 'Test alternative text',
     product_name: 'HP SPECTRE X360',
@@ -26,7 +26,7 @@ const productData = [
     stock: 12,
   },
   {
-    description: 'Test',
+    description: 'Umbrella, a portable, hand-held device that is used for protection against rain and sunlight. The modern umbrella consists of a circular fabric or plastic screen stretched over hinged ribs that radiate from a central pole.',
     product_img: 'https://www.scullyandscully.com/productimages/image.axd/f.82214/w.500/h.500/malacca+umbrella%2c+black.jpg',
     alt: 'Test alternative text',
     product_name: 'Umbrella',
