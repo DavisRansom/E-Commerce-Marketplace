0763a13a360fd1c4ece8af76463ac6b3c6c1cf0d
Davis Ransom <davisransom.vb@gmail.com>
2022-01-03 21:13:30 -0800

    Added Stripe

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,12 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL, process.env.STRIPE_SECRET_KEY);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    process.env.STRIPE_SECRET_KEY,
     {
       host: 'localhost',
       dialect: 'mysql',
