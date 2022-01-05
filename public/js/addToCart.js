0932294a59b88098550b7f929943328e1b89a95f
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-01-05 01:17:12 -0800

    Loaded the items to checkout page

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -46,3 +46,22 @@ if (cartObject === null) {
 cartSizeEl.textContent = cartSizeValue;
 
 $('.add-to-cart').on('click', addToCart);
+
+// Grab data from localstorage
+const products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
+const tableBody = document.querySelector(".table-body");
+
+products.forEach(product => {
+    const tr = document.createElement("tr");
+    tr.classList.add("cart-row")
+    tr.innerHTML = `
+    <td scope="row"><img src="${product.product_img}" 
+    class="cart-img" alt="${product.alt}" width="100px" height="100px"> ${product.product_name}</td>
+    <td>$ ${product.price}</td> 
+    <td>
+      <input class="form-input text-center" type="number" id="product-quantity" name="product-quantity"/>
+    </td>
+    <td><i class="fas fa-trash-alt text-danger delete-cart-item"></i></td>
+    `
+    tableBody.appendChild(tr)
+})
